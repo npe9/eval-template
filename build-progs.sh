0ddd59a30d2cf8974080378d58b6c8a29474573d
Noah Evans <nevans@sandia.gov>
2017-03-20 14:51:32 -0600

    build minimd

--- build-progs.sh
+++ build-progs.sh
@@ -4,6 +4,23 @@ TYPE=$1
 BIN=~/src/ross-paper/bin
 
 source util/setchplenv.bash >/dev/null 2>&1
+
+cd examples/benchmarks/miniMD/
+make
+cp miniMD $BIN/miniMD-$TYPE
+cd ../../../
+exit 0
+
+cd test/studies/graph500/v2/
+chpl --fast *.chpl -o $BIN/graph500v2-$TYPE
+cd ../../../../
+
+cd test/release/examples/benchmarks/ssca2
+make clean
+make "DO_PRINT_TIMING_STATISTICS=1"
+cp SSCA2_main $BIN/SSCA2_main-$TYPE
+cd ../../../../../
+
 cd examples/programs
 chpl --fast quicksort.chpl -o $BIN/quicksort-$TYPE
 cd ../../
@@ -19,6 +36,9 @@ cd ../../../../../
 cd examples/benchmarks/hpcc/
 chpl --fast ra.chpl -o $BIN/ra-$TYPE
 cd ../../../
+
+
+
 
 ### so it's actually a triple.
 ### I will script this eventually.
