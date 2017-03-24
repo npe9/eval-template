71910d1f415a8da893cc75e835a336633f62c4d0
Noah Evans <nevans@sandia.gov>
2017-03-24 10:00:55 -0600

    new iteration of SSCA2

--- SSCA2.sh
+++ SSCA2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo scheduler mem size real user sys
-SIZE=`awk 'BEGIN {for(i=2; i<15;i++) print i}'`
-for i in mutexfifo nemesis distrib; do for j in $SIZE; do echo -n $i hbm $j" "; srun --partition=knl-alpha bash -c "TIMEFORMAT='%R %U %S'; time numactl -m1 bin/SSCA2_main-$i --SCALE=$j 2>&1 1>/dev/null" ; done; done 2>&1
-for i in mutexfifo nemesis distrib; do for j in $SIZE; do echo -n $i ddr $j" "; srun --partition=knl-alpha bash -c "TIMEFORMAT='%R %U %S'; time numactl -m0 bin/SSCA2_main-$i --SCALE=$j 2>&1 1>/dev/null" ; done; done 2>&1
-for i in mutexfifo nemesis distrib; do for j in $SIZE; do echo -n $i cache $j" "; srun --partition=knl-delta bash -c "TIMEFORMAT='%R %U %S'; time numactl bin/SSCA2_main-$i --SCALE=$j 2>&1 1>/dev/null" ; done; done 2>&1
+SIZE=`awk 'BEGIN {for(i=2; i<2**9;i*=2) print i}'`
+SCALE=14
+for i in mutexfifo nemesis distrib; do for j in $SIZE; do echo -n $i hbm $j" "; srun --partition=knl-alpha bash -c "TIMEFORMAT='%R %U %S'; time numactl -m1 bin/SSCA2_main-$i --SCALE=$SCALE --TEST_TORUS_1D=true --TEST_TORUS_2D=true --TEST_TORUS_3D=true --TEST_TORUS_4D=true --dataParTasksPerLocale=$j 2>&1 1>/dev/null" ; done; done 2>&1
+for i in mutexfifo nemesis distrib; do for j in $SIZE; do echo -n $i ddr $j" "; srun --partition=knl-alpha bash -c "TIMEFORMAT='%R %U %S'; time numactl -m0 bin/SSCA2_main-$i --SCALE=$SCALE --TEST_TORUS_1D=true --TEST_TORUS_2D=true --TEST_TORUS_3D=true --TEST_TORUS_4D=true --dataParTasksPerLocale=$j 2>&1 1>/dev/null" ; done; done 2>&1
+for i in mutexfifo nemesis distrib; do for j in $SIZE; do echo -n $i cache $j" "; srun --partition=knl-delta bash -c "TIMEFORMAT='%R %U %S'; time numactl bin/SSCA2_main-$i --SCALE=$SCALE --TEST_TORUS_1D=true --TEST_TORUS_2D=true --TEST_TORUS_3D=true --TEST_TORUS_4D=true --dataParTasksPerLocale=$j 2>&1 1>/dev/null" ; done; done 2>&1
